55f38f880d95ee417708c40649ce4a6cd56a69db
Yudjerick <91619830+Yudjerick@users.noreply.github.com>
2022-12-13 23:12:32 +0300

    add code and example makefile

---
+++ Makefile
@@ -0,0 +1,31 @@
+dress: trousers shoes jacket
+	@echo "dress" > dress
+	@echo "All done. Let's go outside!"
+
+jacket: pullover
+    @echo "Putting on jacket."
+	@echo "pullover" > jacket
+
+pullover: shirt
+	@echo "Putting on pullover."
+	@echo "pullover" > pullover
+
+shirt:
+	@echo "Putting on shirt."
+	@echo "pullover" > shirt
+
+trousers: underpants
+	@echo "Putting on trousers."
+	@echo "pullover" > trousers
+
+underpants:
+	@echo "Putting on underpants."
+	@echo "pullover" > underpants
+
+shoes: socks
+	@echo "Putting on shoes."
+	@echo "pullover" > shoes
+
+socks: pullover
+	@echo "Putting on socks."
+	@echo "pullover" > socks
---
+++ make.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import hashlib
+import json
+
+if __name__ == '__main__':
+    if len(sys.argv)!=2:
+        print("Invalid argument")
+        sys.exit()
+    else:
+        target = sys.argv[1]
+
+if('Makefile' in os.listdir()):
+    makefile = open('Makefile').read()
+else:
+    print('Makefile not found')
+    sys.exit()
+    
+makefilejson = {}
+
+for i in makefile.split('\n'):
+    if len(i) == 0:
+        continue
+    if i.startswith('    ') or i[0] == '\t':
+        makefilejson[cur_target]["commands"].append(i.replace('    ','').replace('\t',''))
+    else:
+        cur_target = i.split()[0].replace(':','')
+        dependencies = i.split()
+        dependencies.pop(0)
+        if(':' in dependencies):
+            dependencies.remove(':')
+        makefilejson[cur_target] = {"commands" : [], "dependencies": dependencies}
+
+if('hashes.txt' in os.listdir()):
+    fileshashes = json.loads(open('hashes.txt').read())
+else:
+    fileshashes = {}
+
+def make(target):
+    need_to_change = False
+    for i in makefilejson[target]["dependencies"]:
+        r = make(i)
+        if(not(need_to_change)):
+            need_to_change = r
+    if target in os.listdir() and target in fileshashes and not(need_to_change):
+        if hashlib.sha1(open(target).read().encode()).hexdigest() == fileshashes[target]:
+            return False
+    for i in makefilejson[target]["commands"]:
+        os.system(i)
+    if target in os.listdir():
+        fileshashes[target] = hashlib.sha1(open(target).read().encode()).hexdigest()
+    return True
+
+make(target)
+f = open('hashes.txt', 'w')
+f.write(json.dumps(fileshashes))
